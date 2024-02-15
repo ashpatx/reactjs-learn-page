26b2926a339310bd27df0b5e83fed7d9c37a5cc4
ashpatx <thingslearn3@gmail.com>
2024-02-15 13:07:43 -0500

    fixed gradient JS background

--- learn-page/src/App.jsx
+++ learn-page/src/App.jsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { Gradient } from 'whatamesh';
+import { Gradient } from "./assets/gradient";
 import Hero from './components/Hero';
 import Generator from './components/Generator';
 import Learn from './components/Learn';
 
 function App() {
+
   useEffect(() => {
     const gradient = new Gradient();
-    gradient.initGradient("#gradient-canvas");
-  }, []); // Empty dependency array to ensure the effect runs only once
+    gradient.initGradient('#gradient-canvas');
+  }, []);
 
   return (
-    <main className='min-h-screen flex flex-col bg-[#F7EBD1] text-black text-sm sm:text-base relative'>
-      <canvas id="gradient-canvas">
-        
-        </canvas> 
-        <Hero className='z-99'/>
+    /*<main className='min-h-screen flex flex-col bg-[#F7EBD1] text-black text-sm sm:text-base relative'>
+      <Hero className='z-99'/>
       <Generator />
-      <Learn />
+      <Learn />*/
+
+      <main className="min-h-screen flex flex-col relative bg-slate-900">
+   <div className="relative z-10 flex flex-col flex-1 justify-center max-w-6xl p-10">
+      <Hero className="flex justify-center text-black" />
+   </div>
+
+      <canvas id="gradient-canvas"  data-transition-in />
     </main>
   );
 }
--- learn-page/src/components/Hero.jsx
+++ learn-page/src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
-      <p>It's time to learn</p>
-      <h1>something <span>new</span></h1>
-      <p>Embarking on the journey of learning something <span>new</span> opens doors to <span>personal growth</span>, <span>resilience</span>, and <span>fulfillment</span>. It stimulates the mind, expands horizons, and fosters adaptability. Whether it's mastering a new language, honing a skill, or exploring a passion, learning <span>enriches life in profound ways</span>. It enhances cognitive abilities, promotes well-being, and offers opportunities for <span>self-discovery</span> in an ever-evolving world.</p>
+    <div className='flex flex-col text-left items-start justify-start gap-2 max-w-screen-lg mx-auto'>
+      <h1 className='text-6xl'>It's time to learn</h1>
+      <h1 className='text-4xl'>something <span>new</span></h1>
+      <p className='text-2xl'>Embarking on the journey of learning something <span>new</span> opens doors to <span>personal growth</span>, <span>resilience</span>, and <span>fulfillment</span>. It stimulates the mind, expands horizons, and fosters adaptability. Whether it's mastering a new language, honing a skill, or exploring a passion, learning <span>enriches life in profound ways</span>. It enhances cognitive abilities, promotes well-being, and offers opportunities for <span>self-discovery</span> in an ever-evolving world.</p>
     </div>
   )
 }
